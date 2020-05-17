da38b25cbbc10c1dd9ba6d480ef516bd87193d1f
Marcelo Monteiro <monteiromarceloc@gmail.com>
2020-05-17 20:54:42 -0300

    upload and map csv

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
+import multer from 'multer';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 import DeleteTransactionService from '../services/DeleteTransactionService';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
+import ImportTransactionsService from '../services/ImportTransactionsService';
+
+import uploadConfig from '../config/upload';
+
+const upload = multer(uploadConfig);
 
 const transactionsRouter = Router();
 
@@ -28,9 +33,14 @@ transactionsRouter.delete('/:id', async (request, response) => {
   return response.status(204).send();
 });
 
-// transactionsRouter.post('/import', async (request, response) => {
-//   const deleteTransaction = new DeleteTransactionService();
-
-// });
+transactionsRouter.post(
+  '/import',
+  upload.single('file') as any,
+  async (request, response) => {
+    const importTransactions = new ImportTransactionsService();
+    const transactions = await importTransactions.execute(request.file.path);
+    return response.json(transactions);
+  },
+);
 
 export default transactionsRouter;
--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,69 @@
+import fs from 'fs';
+import csvParse from 'csv-parse';
+import { getRepository, In, getCustomRepository } from 'typeorm';
+
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface CSVTransactions {
+  title: string;
+  type: string;
+  value: string;
+  category: string;
+}
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(filePath: string): Promise<Transaction[]> {
+    const contactsReadStrem = fs.createReadStream(filePath);
+    const parsers = csvParse({ from_line: 2 });
+    const parseCSV = contactsReadStrem.pipe(parsers);
+
+    const transactions: CSVTransactions[] = [];
+    const categories: string[] = [];
+
+    // Validate fields
+    parseCSV.on('data', async line => {
+      const [title, type, value, category] = line.map((cell: string) =>
+        cell.trim(),
+      );
+      if (!title || !type || !value || !category) return;
+
+      categories.push(category);
+      transactions.push({ title, type, value, category });
+    });
+    await new Promise(resolve => parseCSV.on('end', resolve));
+
+    // Check if exists and create Categories
+    const categoryRepo = getRepository(Category);
+    const existingCategories = await categoryRepo.find({
+      where: { title: In(categories) },
+    });
+    const existingCategoriesTitles = existingCategories.map(e => e.title);
+    const categoryTitlesToAdd = categories
+      .filter(e => !existingCategoriesTitles.includes(e))
+      .filter((value, index, self) => self.indexOf(value) === index); // remove duplicates
+
+    const newCategories = categoryRepo.create(
+      categoryTitlesToAdd.map(title => ({ title })),
+    );
+    await categoryRepo.save(newCategories);
+    const allCategories = [...newCategories, ...existingCategories];
+
+    // Create transactions
+    const transactionsRepo = getCustomRepository(TransactionsRepository);
+    const createdTransactions = transactionsRepo.create(
+      transactions.map(({ title, type, value, category }) => ({
+        title,
+        type,
+        value,
+        category: allCategories.find(cat => cat.title === category),
+      })) as any,
+    );
+
+    await transactionsRepo.save(createdTransactions);
+    await fs.promises.unlink(filePath);
+    return createdTransactions;
   }
 }
 
